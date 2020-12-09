9b92c54e82567dc598d7ea0605c2e4e3bc4b3899
emrdagkusu <emrdagkusu@E.D>
2020-12-09 18:28:34 +0300

    lab6_1 solved

--- lab6_1/Q1.java
+++ lab6_1/Q1.java
@@ -5,50 +5,38 @@ import java.util.Stack;
 
 public class Q1 {
     public static void implementPermutation(int N) {
-//        size is the number of element of what we have got inside the finished stack (finishStack)
-        int size = 1;
-        for (int i = 1; i <= N; i++) {
-            size *= i;
-        }
-
         Stack<String> beginStack = new Stack();
-        Stack<String> finishStack = new Stack();
 
 //        Starting with empty string
-        String temp = "";
-        beginStack.push(temp);
+        String curr = "";
+        beginStack.push(curr);
 
-//        After finishStack has size of number of permutations, we are done
-        while (finishStack.size() != size) {
-            temp = beginStack.pop();
-//            If the temp has N many element inside the string that means this permutation is done
-//            So we can send it to the finishStack
-            if (temp.length() == N) {
-                finishStack.push(temp);
+//        After stack has no element inside it, we are done
+        while (beginStack.size() != 0) {
+            curr = beginStack.pop();
+//            If the current string has N many element inside it, that means this permutation is done
+//            So we can print it
+            if (curr.length() == N) {
+                System.out.println(curr);
             } else {
-//                Control of the current temp has which elements inside it
+//                Control of the curr string for which elements includes inside it
+//                For example if we have curr = "32", we will try to find which one is missing
+//                Then we will push the curr+i which means "321" to the stack again, and so on
                 for (int i = 1; i < N+1; i++) {
-//                    If the temp does not contain the current element we will add that element
-//                    And we will push that temp the beginStack again
-                    if (!temp.contains(""+i)) {
-                        beginStack.push(temp+i);
+//                    If the curr does not contain the current i we will add that element
+//                    And we will push curr the stack again
+                    if (!curr.contains(""+i)) {
+                        beginStack.push(curr+i);
                     }
                 }
             }
         }
-//       To obtain the same order of your output, pop elements to an array
-        String[] stringArray = new String[size];
-        for (int i = 0; i < size; i++) {
-            stringArray[i] = finishStack.pop();
-        }
-//        Printing the elements as seen in your output order
-        for (int i = size-1; i > -1; i--) {
-            System.out.println(stringArray[i]);
-        }
     }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 //        Getting input value
+        System.out.print("Enter integer value for N: ");
         int N = scan.nextInt();
         implementPermutation(N);
     }

d34a880abf79697b56997c4724e55331e4e3922a
emrdagkusu <emrdagkusu@E.D>
2020-12-11 21:30:48 +0300

    lab7 pdf file added

---
+++ lab7/Node.java
@@ -0,0 +1,35 @@
+package lab7;
+
+public class Node {
+    public Node left;
+    public Node right;
+    public int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
